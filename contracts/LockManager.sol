e3b6aa54501d373e6a0bc139b8f17aed0705a05b
AlexeySadkovich <sadkovich01@mail.ru>
2022-02-07 23:01:55 +0300

    feat: add usdc lock

--- contracts/LockManager.sol
+++ contracts/LockManager.sol
@@ -9,19 +9,61 @@ contract LockManager is Initializable {
   // @dev Orders of variables must not be changed!
   // Only additions to the end allowed. 
   // See https://docs.openzeppelin.com/upgrades-plugins/1.x/proxies
+  address internal usdcContractAddress;
+
+  bytes4 private ERC20TransferSelector;
+
   mapping(address => uint256) locked; 
+  mapping (string => bool) operations;
   
   using SafeMathUpgradeable for uint256;
   using ECDSAUpgradeable for bytes32;
 
+  event USDCLocked(
+    address user,
+    uint256 amount,
+    string destination
+  );
+
+  event AVAXLocked(
+    address user,
+    uint256 amount,
+    string destination
+  );
+
   // @dev initialize acts like constructor.
   // This function can only be called once. 
   function initialize() public initializer {
+    usdcContractAddress = 0xA7D7079b0FEaD91F3e65f86E8915Cb59c1a4C664;
 
+    ERC20TransferSelector = bytes4(keccak256(bytes("transferFrom(address,address,uint256")));
+  }
+ 
+  function lockAVAX(string calldata _destination) public payable returns (bool success) {
+    require(msg.value > 0, "Value must be greater than 0");
+
+    locked[msg.sender] = msg.value;
+
+     emit AVAXLocked(msg.sender, msg.value, _destination);
+
+     return true;
   }
 
-  function lock() public payable {
-    require(msg.value > 0, "Not enough amount");
-    locked[msg.sender] = msg.value;
+  function lockUSDC(uint256 _amount, string calldata _destination) external returns (bool success) {
+    require(_amount > 0, "Amount must be greater than 0");
+
+    _transferERC20(usdcContractAddress, msg.sender, address(this), _amount);
+
+    emit USDCLocked(msg.sender, _amount, _destination);
+
+    return true;
+  }
+
+  function _transferERC20(address _token, address _from, address _to, uint256 _amount) private {
+    bytes memory fn = abi.encodeWithSelector(ERC20TransferSelector, _from, _to, _amount);
+
+    (bool success, bytes memory data) = _token.call(fn);
+
+    require(success && (data.length == 0 || abi.decode(data, (bool))), "Transfer failed");
   }
 }
