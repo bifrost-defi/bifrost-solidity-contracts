59bb5d719bed4e7057dcd9918faae58c1027561b
AlexeySadkovich <sadkovich01@mail.ru>
2022-02-19 21:43:34 +0300

    feat: add operations approving

--- contracts/LockManager.sol
+++ contracts/LockManager.sol
@@ -2,27 +2,34 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 import "@openzeppelin/contracts-upgradeable/utils/math/SafeMathUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/cryptography/ECDSAUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-contract LockManager is Initializable {
+contract LockManager is Initializable, OwnableUpgradeable {
   // @dev Orders of variables must not be changed!
   // Only additions to the end allowed. 
   // See https://docs.openzeppelin.com/upgrades-plugins/1.x/proxies
-  address internal usdcContractAddress;
-
   bytes4 private ERC20TransferSelector;
 
   mapping(address => uint256) locked; 
-  mapping (string => bool) operations;
+  mapping(address => Operation) operations;
   
+  address internal usdcContractAddress;
+
   using SafeMathUpgradeable for uint256;
   using ECDSAUpgradeable for bytes32;
+
+   event PreCommit(
+    address user,
+    string destination
+  );
 
   event USDCLocked(
     address user,
     uint256 amount,
-    string destination
+    string destination,
+    string operationID
   );
 
   event USDCUnlocked(
@@ -33,7 +40,8 @@ contract LockManager is Initializable {
   event AVAXLocked(
     address user,
     uint256 amount,
-    string destination
+    string destination,
+    string operationID
   );
 
   event AVAXUnlocked(
@@ -41,20 +49,63 @@ contract LockManager is Initializable {
     uint256 amount
   );
 
+  struct Operation {
+    bool approved;
+    string id;
+    uint256 amount;
+    string destination;
+  }
+
+  modifier operationApproved {
+    require(
+      operations[msg.sender].approved,
+      "Operation was not approved"
+    );
+    require(
+      keccak256(bytes(operations[msg.sender].id)) != keccak256(bytes("")),
+      "Operation has no id"
+    );
+    _;
+  }
+
   // @dev initialize acts like constructor.
   // This function can only be called once. 
   function initialize() public initializer {
+    __Ownable_init();
+
     usdcContractAddress = 0xA7D7079b0FEaD91F3e65f86E8915Cb59c1a4C664;
 
     ERC20TransferSelector = bytes4(keccak256(bytes("transferFrom(address,address,uint256)")));
   }
 
-  function lockUSDC(uint256 _amount, string calldata _destination) external returns (bool success) {
+  function createOperation(uint256 _amount, string calldata _destination) public {
+    require(_amount > 0, "Amount must be greater than 0");
+    
+    operations[msg.sender] = Operation(false, "", _amount, _destination);
+
+    emit PreCommit(msg.sender, _destination);
+  }
+
+  function approve(string memory _id, address _user, uint256 _amount, string calldata _destination) public onlyOwner {
+    Operation memory op = operations[_user];
+
+    require(!op.approved, "Operation already approved");
+    require(keccak256(bytes(op.id)) == keccak256(bytes("")), "Operation id is not empty");
+    require(op.amount == _amount, "Operation amount has changed");
+    require(keccak256(bytes(op.destination)) == keccak256(bytes(_destination)), "Operation destination has changed");
+
+    operations[msg.sender].approved = true;
+    operations[msg.sender].id = _id;
+  }
+
+  function lockUSDC(uint256 _amount, string calldata _destination) operationApproved external returns (bool success) {
     require(_amount > 0, "Amount must be greater than 0");
 
     _transferERC20(usdcContractAddress, msg.sender, address(this), _amount);
 
-    emit USDCLocked(msg.sender, _amount, _destination);
+    emit USDCLocked(msg.sender, _amount, _destination, operations[msg.sender].id);
+
+    delete operations[msg.sender];
 
     return true;
   }
@@ -69,14 +120,16 @@ contract LockManager is Initializable {
     return true;
   }
 
-  function lockAVAX(string calldata _destination) public payable returns (bool success) {
+  function lockAVAX(string calldata _destination) operationApproved public payable returns (bool success) {
     require(msg.value > 0, "Value must be greater than 0");
 
     locked[msg.sender] = msg.value;
 
-     emit AVAXLocked(msg.sender, msg.value, _destination);
+    emit AVAXLocked(msg.sender, msg.value, _destination, operations[msg.sender].id);
 
-     return true;
+    delete operations[msg.sender];
+
+    return true;
   }
 
   function unlockAVAX(uint256 _amount) public returns (bool success) {
