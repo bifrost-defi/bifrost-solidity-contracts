d49427073c79ae1133aa7cee294240789e004c60
AlexeySadkovich <sadkovich01@mail.ru>
2022-03-08 14:43:57 +0300

    build: update truffle config

--- migrations/2_deploy_lock_manager.js
+++ migrations/2_deploy_lock_manager.js
@@ -1,8 +1,11 @@
+require("dotenv").config();
 const { deployProxy } = require("@openzeppelin/truffle-upgrades");
 
 const LockManager = artifacts.require("LockManager");
 
 module.exports = async function (deployer) {
-  const instance = await deployProxy(LockManager, { deployer });
+  const instance = await deployProxy(LockManager, [process.env.USDC_CONTRACT], {
+    deployer,
+  });
   console.log("Deployed", instance.address);
 };
--- truffle-config.js
+++ truffle-config.js
@@ -1,47 +1,24 @@
-/**
- * Use this file to configure your truffle project. It's seeded with some
- * common settings for different networks and features like migrations,
- * compilation and testing. Uncomment the ones you need or modify
- * them to suit your project as necessary.
- *
- * More information about configuration can be found at:
- *
- * trufflesuite.com/docs/advanced/configuration
- *
- * To deploy via Infura you'll need a wallet provider (like @truffle/hdwallet-provider)
- * to sign your transactions before they're sent to a remote public node. Infura accounts
- * are available for free at: infura.io/register.
- *
- * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
- * public/private key pairs. If you're publishing your code to GitHub make sure you load this
- * phrase from a file you've .gitignored so it doesn't accidentally become public.
- *
- */
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+require("dotenv").config();
+
+const localPort = 26171;
+const localURL = `http://127.0.0.1:${localPort}/ext/bc/C/rpc`;
+
+let mnemonic = process.env.PRIVATE_KEY;
 
 module.exports = {
-  /**
-   * Networks define how you connect to your ethereum client and let you set the
-   * defaults web3 uses to send transactions. If you don't specify one truffle
-   * will spin up a development blockchain for you on port 9545 when you
-   * run `develop` or `test`. You can ask a truffle command to use a specific
-   * network from the command line, e.g
-   *
-   * $ truffle test --network <network-name>
-   */
-
   networks: {
     development: {
-      host: "127.0.0.1", // Localhost (default: none)
-      port: 8545, // Standard Ethereum port (default: none)
-      network_id: "*", // Any network (default: none)
+      provider: () => {
+        return new HDWalletProvider(mnemonic, localURL);
+      },
+      network_id: "*",
+      gas: 3000000,
+      gasPrice: 225000000000,
     },
     testnet: {
-      host: "https://api.avax-test.network/ext/bc/C/rpc",
+      host: "api.avax-test.network/ext/bc/C/rpc",
       network_id: "*",
     },
   },
